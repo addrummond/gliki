721b374bc9e46b256e7ac4eeaf609946f2382f03
Alex Drummond <a.d.drummond@gmail.com>
2007-05-28 22:18:08 +0100

    Add AES implementation for tlslite library for encrypting passwords in DB.

---
+++ aes/__init__.py
@@ -0,0 +1,7 @@
+"""The pure Python AES implementation taken from the tlslite library."""
+
+#
+# All code in the aes directory is taken from the tlslite library, which is
+# in the public domain.
+#
+
