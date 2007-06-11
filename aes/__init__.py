8d183c0090accfcdffa5cc61ec559e623435627d
Alex Drummond <a.d.drummond@gmail.com>
2007-06-11 17:22:10 +0100

    Clarify licensing of AES code.

--- aes/__init__.py
+++ aes/__init__.py
@@ -1,8 +1,10 @@
 """The pure Python AES implementation taken from the tlslite library."""
 
 #
-# All code in the aes directory is taken from the tlslite library, which is
-# in the public domain.
+# All code in the 'aes' directory is taken from the tlslite library, which is
+# in the public domain. Therefore, the code in this directory need not be
+# distributed under the terms of the GPL license which covers the rest of the
+# Gliki code.
 #
 # See http://trevp.net/tlslite/
 #
